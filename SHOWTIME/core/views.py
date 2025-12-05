73bbb732d636904f845167fdeaab042f9d26d2c2
brunocdantas <brunocdantas2006@gmail.com>
2025-12-05 11:03:56 +0000

    Remove espaços em branco desnecessários no arquivo views.py

--- SHOWTIME/core/views.py
+++ SHOWTIME/core/views.py
@@ -7,21 +7,17 @@ from django.contrib.auth.decorators import login_required
 def home(request):
     return render(request, 'core/home.html')
 
-
 def usuario(request):
     if request.method == "POST":
         return redirect('home')
     return render(request, 'core/usuario.html')
 
-
 @login_required
 def favoritos(request):
     return render(request, 'core/favoritos.html')
 
-
 def artistas(request):
     return render(request, 'core/artistas.html')
-
 
 def custom_logout(request):
     """Faz logout e volta para a home."""
