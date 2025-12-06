f44bfa550634698c6c6cd6672df72c6437895b18
brunocdantas <brunocdantas2006@gmail.com>
2025-12-06 00:29:24 +0000

    temos artistas

--- SHOWTIME/core/views.py
+++ SHOWTIME/core/views.py
@@ -2,6 +2,7 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import logout
 from django.contrib.auth.decorators import login_required
+from .models import Artist
 
 
 def home(request):
@@ -23,3 +24,22 @@ def custom_logout(request):
     """Faz logout e volta para a home."""
     logout(request)         
     return redirect('home') 
+
+
+def coringa_page(request):
+    query = request.GET.get("q", "").strip()
+
+    if query == "":
+        return render(request, "core/artist_not_found.html")
+
+    artist = Artist.objects.filter(name__icontains=query).first()
+
+    if not artist:
+        return render(request, "core/artist_not_found.html", {"query": query})
+
+    top_musics_list = [x.strip() for x in artist.top_musics.split(",")]
+
+    return render(request, "core/coringa.html", {
+        "artist": artist,
+        "top_musics": top_musics_list,
+    })
